2160cf95a178223f1ff61b4febc5c33f33d1dd55
Ahmed Amir Salem (Kenway The Great) <ahmad_amir@live.com>
2020-08-19 02:54:18 +0200

    Updates

---
+++ precache-manifest.fddaf38f529186ef2e05a04b3b24f8f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aec1b0aef36edb92126de489770239ac",
+    "url": "/film-crow-project/index.html"
+  },
+  {
+    "revision": "f7e8d104dc0331504938",
+    "url": "/film-crow-project/static/css/main.6e819422.chunk.css"
+  },
+  {
+    "revision": "7b80d22365f5fe17f79c",
+    "url": "/film-crow-project/static/js/2.3f65c9aa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/film-crow-project/static/js/2.3f65c9aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7e8d104dc0331504938",
+    "url": "/film-crow-project/static/js/main.5b12a798.chunk.js"
+  },
+  {
+    "revision": "42b608a50af06202b466",
+    "url": "/film-crow-project/static/js/runtime-main.1e1d5364.js"
+  }
+]);
